52447c7b692dad563f34050ee4218d088cf9b4ad
trcruse <trcruse@F1092-26-2114>
2019-12-02 16:45:22 -0600

    LINQ movie site

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -72,5 +72,10 @@ namespace Movies
 
             return results;
         }
+
+        public static List<Movie> Sort(List<Movie> movies)
+        {
+            return movies.Sort()
+        }
     }
 }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,7 +9,11 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<Movie> Movies;
+
+        /// <summary>
+        /// Gives a list, but generalized so can be used with any IEnumberable
+        /// </summary>
+        public IEnumerable<Movie> Movies;
 
         [BindProperty]
         public string search { get; set; }
@@ -23,11 +27,13 @@ namespace Movies.Pages
         [BindProperty]
         public float? maxIMDB { get; set; }
 
-
+        [BindProperty]
+        public string sort { get; set; }
 
         public void OnGet()
         {
             Movies = MovieDatabase.All;
+            Movies = Movies.OrderBy(movie => movie.Title);
         }
 
         public void OnPost()
@@ -36,20 +42,60 @@ namespace Movies.Pages
 
             if (search != null)
             {
-                Movies = MovieDatabase.Search(Movies, search);
+                //See if matches a criteria
+                Movies = Movies.Where(movie => movie.Title.Contains(search, StringComparison.OrdinalIgnoreCase));
+
+                //Movies = MovieDatabase.Search(Movies, search);
             }
 
             if(mpaa.Count != 0)
             {
-                Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
+                Movies = Movies.Where(movie => mpaa.Contains(movie.MPAA_Rating));
+
+                //Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
             }
 
             if(minIMDB != null)
             {
-                Movies = MovieDatabase.FilterByMinIMDB(Movies, (float)minIMDB);
+                //nullable object from MovieDatabase
+                Movies = Movies.Where(movie => movie.IMDB_Rating !=null && movie.IMDB_Rating >= minIMDB);
+
+               //Movies = MovieDatabase.FilterByMinIMDB(Movies, (float)minIMDB);
             }
 
+            if(maxIMDB != null)
+            {
+                Movies = Movies.Where(movie => movie.IMDB_Rating != null && movie.IMDB_Rating <= maxIMDB);
+            }
+
+            switch (sort)
+            {
+                case "title":
+                    Movies = Movies.OrderBy(movie => movie.Title);
+                    break;
+                case "director":
+                    Movies = Movies
+                        .Where(movie => movie.Director != null)
+                        .OrderBy(movie =>
+                        {
+                            string[] parts = movie.Director.Split(" ");
+                            return parts[parts.Length - 1];
+                        });
+                    break;
+                case "mpaa":
+                    Movies = Movies.OrderBy(movie => movie.MPAA_Rating);
+                    break;
+                case "imdb":
+                    Movies = Movies.OrderBy(movie => movie.IMDB_Rating);
+                    break;
+                case "rt":
+                    Movies = Movies
+                        .Where(movie => movie.Rotten_Tomatoes_Rating != null)
+                        .OrderBy(movie => movie.Rotten_Tomatoes_Rating);
+                    break;
+            }
             
+
         }
     }
 }
